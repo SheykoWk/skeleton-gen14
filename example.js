6a0bbacc4b14815f227ecae66511dd6147d0c4d4
Sahid Kick <sh3yk0@localhost.localdomain>
2022-08-15 19:54:38 -0500

    add edit controler

---
+++ example.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+
+const myFunc = () => {
+  return sum(2, 7); //? retorna a + b de la funcion sum
+};
+
+console.log(myFunc());
+
+{
+    
+}
--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -1,43 +1,62 @@
-const uuid = require('uuid')
-const { comparePassword, hashPassword} = require('../utils/crypt')
+const uuid = require("uuid");
+const { hashPassword } = require("../utils/crypt");
 
-
-const userDB = []
-
+const userDB = [];
 
 const getAllUsers = () => {
-    return userDB
-    //? select * from users;
-}
+  return userDB;
+  //? select * from users;
+};
 
 const getUserById = (id) => {
-    const data = userDB.filter(item => item.id === id)
-    return data[0]
-    //? select * from users where id = ${id};
-}
+  const data = userDB.filter((item) => item.id === id);
+  return data[0];
+  //? select * from users where id = ${id};
+};
 
 const createUser = (data) => {
-    const newUser = {
-        id: uuid.v4(), //obligatorio y unico
-        first_name: data.first_name, //obligatorio
-        last_name: data.last_name, //obligatorio
-        email: data.email, //obligatorio y unico
-        password: hashPassword(data.password), //obligatorio
-        phone: data.phone ? data.phone : '' , //unico
-        birthday_date: data.birthday_date, //obligatorio
-        rol: 'normal', //obligatorio y por defecto "normal"
-        profile_image: data.profile_image ? data.profile_image : '',
-        country: data.country,  //obligatorio
-        is_active: true, //obligatorio y por defecto true
-        verified: false //obligatorio y por defecto false
-    }
-    console.log(newUser);
-}
-createUser({password: 'root'})
+  const newUser = {
+    id: uuid.v4(), //obligatorio y unico
+    first_name: data.first_name, //obligatorio
+    last_name: data.last_name, //obligatorio
+    email: data.email, //obligatorio y unico
+    password: hashPassword(data.password), //obligatorio
+    phone: data.phone ? data.phone : "", //unico
+    birthday_date: data.birthday_date, //obligatorio
+    rol: "normal", //obligatorio y por defecto "normal"
+    profile_image: data.profile_image ? data.profile_image : "",
+    country: data.country, //obligatorio
+    is_active: true, //obligatorio y por defecto true
+    verified: false, //obligatorio y por defecto false
+  };
+  userDB.push(newUser);
+  return newUser;
+};
+
+const editUser = (id, data) => {
+  const index = userDB.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    userDB[index] = {
+      id: id,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      email: data.email,
+      password: hashPassword(data.password),
+      phone: data.phone, //unico
+      birthday_date: data.birthday_date,
+      rol: data.rol,
+      profile_image: data.profile_image,
+      country: data.country,
+      is_active: data.is_active,
+      verified: false,
+    };
+    return userDB[index];
+  } else {
+    return createUser(data);
+  }
+};
 
 
 
-const suma = (a , b) => a + b
 
-suma(3 , 6)
 
