527d13c828d9c25698a924348a830a03f1a4197a
SheykoWk <sheyko.wk@gmail.com>
2022-08-16 21:59:29 -0500

    add the login controller

--- src/auth/auth.controllers.js
+++ src/auth/auth.controllers.js
@@ -5,9 +5,12 @@ const loginUser = (email, password) => {
     const user = getUserByEmail(email)
     //? user.password Contraseña hasheada
     //* password Contraseña en texto plano
-    const verify_password = comparePassword(password, user.password)
-
-    return verify_password
+    if(user){
+        const verify_password = comparePassword(password, user.password)
+        if(verify_password){
+            return user
+        }
+    }
+    return false
 }
 
-console.log(loginUser('sahid.kick@academlo.com', 'root123'))
